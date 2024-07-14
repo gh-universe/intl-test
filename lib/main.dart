28250533c9acfda0caf88fb1189988abb4f6aefd
Developer1 <dev1@aksahypa.com>
2024-07-14 16:34:36 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
+import 'package:turkmen_localization_support/turkmen_localization_support.dart';
+
+import 'generated/strings.g.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initializeDateFormatting();
+
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TranslationProvider(
+      child: Builder(builder: (context) {
+        return MaterialApp(
+          home: const HomeScreen(),
+          locale: TranslationProvider.of(context).flutterLocale,
+          supportedLocales: AppLocaleUtils.supportedLocales,
+          localizationsDelegates: const [
+            ...TkDelegates.delegates,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          debugShowCheckedModeBanner: false,
+        );
+      }),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Localizations.override(
+      context: context,
+      locale: AppLocale.tk.flutterLocale,
+      delegates: const [
+        ...TkDelegates.delegates,
+      ],
+      child: Scaffold(
+        body: Center(
+          child: Text('${context.translations.hello}, ${DateFormat('dd LLL, yyyy').format(DateTime.now())}'),
+        ),
+      ),
+    );
+  }
+}
